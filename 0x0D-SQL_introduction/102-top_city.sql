36980b4aa8298d4e272518d1a5a7507c8c2848c1
Michael Mwai <michaeldmwai@gmail.com>
2024-03-06 02:11:01 -0800

    Avg temp.

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,6 @@
 --  Script that displays the top 3 of cities temperature during July and August
 USE 'hbtn_0c_0'
-SELECT city, MAX(temperature) AS max_temperature
+SELECT city, AVG(temperature) AS avg_temp
 FROM Temperatures
 WHERE month IN ('July', 'August')
 GROUP BY city
