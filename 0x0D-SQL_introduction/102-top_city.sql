20b69a48effc9c25b9c3dfb5695e5182cc2f33c4
Michael Mwai <michaeldmwai@gmail.com>
2024-03-06 02:05:54 -0800

    Top 3 temperature.

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+--  Script that displays the top 3 of cities temperature during July and August
+USE 'hbtn_0c_0'
+SELECT city, MAX(temperature) AS max_temperature
+FROM Temperatures
+WHERE month IN ('July', 'August')
+GROUP BY city
+ORDER BY max_temperature DESC
+LIMIT 3;
